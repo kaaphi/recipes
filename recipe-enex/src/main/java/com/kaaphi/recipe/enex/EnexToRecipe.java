d8b8857dcca1e849cceacbe3f35e5357d6464897
phil kaasa <phil@kaaphi.com>
2018-05-19 11:36:36 -0500

    WIP More complete Enex processing. There are quite a few failures that can probably be fixed, and ingredient parsing doesn't work correctly, but this is a start.

--- recipe-enex/src/main/java/com/kaaphi/recipe/enex/EnexToRecipe.java
+++ recipe-enex/src/main/java/com/kaaphi/recipe/enex/EnexToRecipe.java
@@ -2,31 +2,63 @@ package com.kaaphi.recipe.enex;
 
 import com.evernote.enex.EnExport;
 import com.evernote.enex.Note;
+import com.kaaphi.recipe.Recipe;
+import com.kaaphi.recipe.RecipeBookEntry;
+import com.kaaphi.recipe.txtformat.IngredientParser;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.transform.stream.StreamSource;
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 public class EnexToRecipe {
+  private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmssX");
   
-  public void processNote(Note note) throws SAXException, IOException, ParserConfigurationException {
-    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-    Document doc = dBuilder.parse(new ByteArrayInputStream(note.getContent().getBytes(Charset.forName("UTF-8"))));
+  public RecipeBookEntry processNote(Note note) throws ParserConfigurationException, SAXException, IOException  {
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+    //factory.setValidating(true);
+    factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+    SAXParser saxParser = factory.newSAXParser();
     
-    //doc.getFirstChild()
+    
+    NoteParser handler = new NoteParser();
+    saxParser.parse(new ByteArrayInputStream(note.getContent().getBytes(Charset.forName("UTF-8"))), handler);
+    
+    IngredientParser ingredientParser = new IngredientParser();
+    
+    Recipe recipe = new Recipe(
+        note.getTitle(), 
+        handler.getIngredients().stream().map(ingredientParser::fromString).collect(Collectors.toList()),
+        handler.getMethod());
+    
+    Instant created = Optional.ofNullable(note.getCreated())
+        .map(txt -> ZonedDateTime.parse(txt, TIME_FORMATTER).toInstant())
+        .orElseGet(Instant::now);
+    Instant updated = Optional.ofNullable(note.getUpdated())
+        .map(txt -> ZonedDateTime.parse(txt, TIME_FORMATTER).toInstant())
+        .orElse(null);
+
+    return new RecipeBookEntry(UUID.randomUUID(), recipe, created, updated);
   }
   
   public EnExport loadExport(File file) throws JAXBException, XMLStreamException {
@@ -42,8 +74,32 @@ public class EnexToRecipe {
     return export;
   }
   
+  public List<RecipeBookEntry> toRecipeBook(EnExport export, BiConsumer<Note, Throwable> failureHandler) {
+    return export.getNote().stream()
+        .map(note -> {
+          try {
+            return processNote(note);
+          } catch (SAXException | ParserConfigurationException | IOException e) {
+            failureHandler.accept(note, e);     
+            return null;
+          }
+        })
+        .filter(Objects::nonNull)
+        .collect(Collectors.toList());
+  }
+  
   
   public static void main(String[] args) throws Exception {
     File file = new File("/Users/kaaphi/Documents/Evernote/Recipes.enex");
+    
+    EnexToRecipe converter = new EnexToRecipe();
+    
+    AtomicInteger failed = new AtomicInteger();
+    converter.toRecipeBook(converter.loadExport(file), (note, exception) -> {
+      System.out.format("Failed: <%s> (%s)%n", note.getTitle(), exception);
+      failed.getAndIncrement();
+    });
+    
+    System.out.println(failed);
   }
 }
