5bc1a465c785364a5faebab9cc41d19d8c71d1cc
phil kaasa <phil@kaaphi.com>
2018-05-19 12:44:04 -0500

    Improve IngredientParser.

--- recipe-core/src/test/java/com/kaaphi/recipe/txtformat/TestIngredientParser.java
+++ recipe-core/src/test/java/com/kaaphi/recipe/txtformat/TestIngredientParser.java
@@ -20,7 +20,13 @@ public class TestIngredientParser {
       {"1 b item", "1 b", "item"},
       {"1 item", "1", "item"},
       {"1item", null, "1item"},
-      {"four items", null, "four items"}
+      {"four items", null, "four items"},
+      {"1/2 cup butter", "1/2 cup", "butter"},
+      {"1/4 - 1/3 cup rice", "1/4 - 1/3 cup", "rice"},
+      {"1.25 scoops stuff", "1.25 scoops", "stuff"},
+      {"1/4 apple", "1/4", "apple"},
+      {"1c. sugar", "1c.", "sugar"},
+      {"1 tbps. sugar", "1 tbps.", "sugar"}
       });
     }
 
