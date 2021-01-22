280c86c01bcca3039a01320341fc9caa382a99c0
phil kaasa <phil@kaaphi.com>
2021-01-22 15:46:43 -0600

    Remove docker container for recipe-admin, general clean up, remove recipe-service since the production deploy is maintained in a separate repo.

--- recipe-service/backup_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker exec $(docker ps --filter name=recipes_db -q) pg_dump -d postgres -U postgres > backup_`date +%Y%m%d`.dump
-
--- recipe-service/restore_db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker exec -i $(docker ps --filter name=recipes_db -q) psql -U postgres --set ON_ERROR_STOP=on postgres
-
