e7f9c1259a4979c7b2a84ea1482baffa3a3271e3
phil kaasa <phil@kaaphi.com>
2021-02-07 15:02:00 -0600

    Support environment variables to define DB connection for tests.

--- recipe-app/src/test/java/com/kaaphi/recipe/app/TestRecipeModule.java
+++ recipe-app/src/test/java/com/kaaphi/recipe/app/TestRecipeModule.java
@@ -12,6 +12,7 @@ import com.kaaphi.recipe.repo.RepoTestHelper.RepoTestTransaction;
 import com.kaaphi.recipe.repo.SingleConnectionDataSource;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Map;
 import javax.sql.DataSource;
 import org.postgresql.ds.PGSimpleDataSource;
 
@@ -23,10 +24,16 @@ public class TestRecipeModule {
         .with(new Module() {
           @Override
           public void configure(Binder binder) {
+            Map<String,String> env = System.getenv();
+            String dbUser = env.getOrDefault("POSTGRES_USER", "postgres");
+            String dbPass = env.getOrDefault("POSTGRES_PASSWORD", "mysecretpassword");
+            String dbHost = env.getOrDefault("POSTGRES_HOST", "localhost");
+            String dbName = env.getOrDefault("POSTGRES_DB", "postgres");
+
             binder.bind(String.class)
                 .annotatedWith(Names.named("dbUrl"))
                 .toInstance(
-                    "jdbc:postgresql://localhost/postgres?user=postgres&password=mysecretpassword");
+                    String.format("jdbc:postgresql://%s/%s?user=%s&password=%s", dbHost, dbName, dbUser, dbPass));
           }
 
           @Provides @Singleton
